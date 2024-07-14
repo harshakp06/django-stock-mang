a49b2cab5797e9ecb392c07467f550135ba56669
Harsha Vardhan <kpharsha@yahoo.com>
2024-07-14 11:58:58 +0530

    add

--- src/stockmgt/admin.py
+++ src/stockmgt/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from .form import StockCreateForm
 
 from .models import Stock
 # Register your models here.
 
-admin.site.register(Stock)
+
+
+class StockCreateAdmin(admin.ModelAdmin):
+    list_display = ['category','item_name','quantity']
+    form = StockCreateForm
+    list_filter = ['category']
+    search_fields = ['category','item_name']
+
+
+admin.site.register(Stock,StockCreateAdmin)
